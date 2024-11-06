c857758d3bdd1f4b52908a903a8c14f7e805699a
tod1az <t.diaz.soto@gmail.com>
2024-11-06 10:49:32 -0300

    add ventas page

---
+++ src/app/ventas/page.tsx
@@ -0,0 +1,9 @@
+import { SalesTable } from "@/components/sales-table";
+
+export default function Page() {
+  return (
+    <main className="flex items-center  justify-center px-[10dvw]">
+      <SalesTable />
+    </main>
+  )
+}
